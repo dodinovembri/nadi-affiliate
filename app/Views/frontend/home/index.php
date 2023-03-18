75d8646e7cccaa4855536eeab9008dfee310a1b3
dodinovembri <dodinovembri32@gmail.com>
2023-03-18 22:14:37 +0700

    update

--- app/Views/frontend/home/index.php
+++ app/Views/frontend/home/index.php
@@ -13,7 +13,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
-
+    <base href="/"/>
 </head>
 
 <body class="theme-light" data-highlight="highlight-red" data-gradient="body-default">
