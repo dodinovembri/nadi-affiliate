9e083c12eb5e92e38bdbdb7d499c70776e65f120
dodinovembri <dodinovembri32@gmail.com>
2023-03-18 23:36:32 +0700

    update

--- app/Views/frontend/home/index.php
+++ app/Views/frontend/home/index.php
@@ -9,30 +9,28 @@
     <title>Inspirasi Stuff</title>
     <link rel="stylesheet" href="<?= base_url('assets/frontend/styles/bootstrap.css') ?>">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i|Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i,900,900i&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?= base_url('assets/frontend/fonts/css/fontawesome-all.min.css') ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="<?= base_url('assets/frontend/fonts/css/fontawesome-all.min.css') ?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 </head>
 
 <body class="theme-light" data-gradient="body-default">
     <div id="page">
         <div class="page-content header-clear-small">
-            <!-- <div class="card card-style pb-3"> -->
-                <div class="accordion mb-2">
-                    <?php foreach ($product_categories as $key => $value) { ?>
-                        <div data-card-height="90" class="card card-style mb-0 mt-2 rounded-s" style="background-image: url(<?= base_url('assets/images/product_category/' . $value->image) ?>);">
-                            <div class="card-center">
-                                <button class="btn accordion-btn">
-                                    <h4 class="text-center color-white text-uppercase"><?= $value->code ?></h4>
-                                    <p class="text-center color-white opacity-70 mb-0 mt-n2"><?= $value->name ?></p>
-                                </button>
-                            </div>
-                            <div class="card-overlay rounded-s bg-black opacity-70"></div>
+            <div class="accordion mb-2">
+                <?php foreach ($product_categories as $key => $value) { ?>
+                    <div class="card card-style mb-2 mt-2 rounded-s" style="background-image: url(<?= base_url('assets/images/product_category/' . $value->image) ?>);"><br><br><br><br>
+                        <div class="card-center">
+                            <button class="btn accordion-btn">
+                                <h4 class="text-center color-white text-uppercase"><?= $value->code ?></h4>
+                                <p class="text-center color-white opacity-70 mb-0 mt-n2"><?= $value->name ?></p>
+                            </button>
                         </div>
-                    <?php } ?>
-                </div>
-            <!-- </div> -->
+                        <div class="card-overlay rounded-s bg-black opacity-70"></div>
+                    </div>
+                <?php } ?>
+            </div>
             <div class="search-page" style="margin-top: 20px;">
                 <div class="search-box search-header bg-theme card-style me-3 ms-3">
                     <i class="fa fa-search"></i>
@@ -46,7 +44,6 @@
         </div>
     </div>
     <script type="text/javascript" src="<?= base_url('assets/frontend/scripts/bootstrap.min.js') ?>"></script>
-    <script type="text/javascript" src="<?= base_url('assets/frontend/scripts/custom.js?v3') ?>"></script>
 </body>
 
 <script>
